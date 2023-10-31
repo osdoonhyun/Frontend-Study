0733fbcb7d5886e9bd5a388cd87b56f425736b53
osdoonhyun <skw1996@naver.com>
2023-10-31 23:59:14 +0900

    feat: infer 내용 추가

--- typescript/utilityTypes/infer.ts
+++ typescript/utilityTypes/infer.ts
@@ -18,3 +18,9 @@ type R<T extends (...args: any) => any> = T extends (...args: any) => infer A
 type Params = Parameters<typeof zip>;
 type Return = ReturnType<typeof zip>;
 type First = Params[2];
+
+// 함수의 매개변수의 타입들을 가져오기 위해 T를 가져와야 하고 이것은 함수여야 한다.
+// 1. T를 함수로 제한한다. <T extends (...args: any) => any>
+// 2. infer는 extends에서만 사용 = T extends (...args: infer A) => any ? A : never
+// infer는 typescript가 알아서 매개변수를 추론하라는 것.
+// 추론한 값이 있으면 쓰고 없으면 쓰지 마라
